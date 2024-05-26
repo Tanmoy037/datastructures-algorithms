d311849ba7e3e801167f8fb394b0807c9c4d39e8
Tanmoy037 <tanmoysantra67@gmail.com>
2024-05-26 22:40:50 +0530

    Q 2485 added

---
+++ leetCode2485.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int pivotInteger(int n) {
+        int[] prefixSum = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            prefixSum[i] = prefixSum[i - 1] + i;
+        }
+        
+        // Iterate to find the pivot integer
+        for (int i = 1; i <= n; i++) {
+            int leftSum = prefixSum[i];  // Sum of numbers from 1 toi
+            int rightSum = prefixSum[n] - prefixSum[i - 1];  // Sum of numbers from i to n
+            
+            if (leftSum == rightSum) {
+                return x;
+            }
+        }
+        
+        return -1;  
+    }
+}
