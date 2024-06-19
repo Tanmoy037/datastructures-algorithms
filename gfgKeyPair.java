431a052c793d6e82b9c4ecc85776d00beb5dd1ec
Tanmoy037 <tanmoysantra67@gmail.com>
2024-06-19 20:59:41 +0530

    two gfg question added

---
+++ gfgKeyPair.java
@@ -0,0 +1,15 @@
+// https://www.geeksforgeeks.org/problems/key-pair5616/1
+
+class Solution {
+    boolean hasArrayTwoCandidates(int arr[], int n, int x) {
+        HashSet<Integer> res = new HashSet<>();
+        for (int num : arr) {
+            int temp = x - num;
+            if (res.contains(temp)) {
+                return true;
+            }
+            res.add(num);
+        }
+        return false;
+    }
+}
---
+++ gfgNonRepeatingElement.java
@@ -0,0 +1,28 @@
+// https://www.geeksforgeeks.org/problems/non-repeating-element3958/1
+class Check{
+    
+    public int firstNonRepeating(int arr[], int n) 
+    { 
+        // Complete the function
+        HashMap <Integer,Integer>hm=new HashMap<>();
+        
+        for(int i=0;i<arr.length;i++){
+            if(hm.containsKey(arr[i])==false){
+                hm.put(arr[i],1);
+            }
+            else{
+                int temp=hm.get(arr[i]);
+                hm.put(arr[i],temp+1);
+            }
+        }
+        
+        for(int i=0;i<arr.length;i++){
+            int a=arr[i];
+            if(hm.get(a)==1){
+                return a;
+            }
+        }
+        return 0;
+    }  
+    
+}
