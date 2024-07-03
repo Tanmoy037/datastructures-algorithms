c2565d539a483205588dfae0496f2db9b27644f4
Tanmoy037 <tanmoysantra67@gmail.com>
2024-07-03 07:20:50 +0530

    q 69 added

---
+++ leetCode69.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int mySqrt(int x) {
+        int low = 0;
+        int high = x;
+        int ans = 0;
+
+        while (low <= high){
+            int mid = low + (high - low) / 2;
+
+            if ((long)mid * mid <= (long)x){
+                ans = mid;
+                low = mid + 1;
+
+            }else if (mid * mid == x){
+                return mid;
+            }else {
+                high = mid - 1; 
+            }
+        }
+
+        return ans;
+    }
+}
