566286a912727a3b5d6b57ecfc7f4702fd4069e1
Tanmoy037 <tanmoysantra67@gmail.com>
2024-07-09 09:42:43 +0530

    Q 155 added

---
+++ leetCode155.java
@@ -0,0 +1,37 @@
+// solution can be ve optimized in O(1) space
+import java.util.Stack;
+
+class MinStack {
+    Stack <Integer> st = new Stack<>();
+    Stack <Integer> min = new Stack<>(); 
+
+    public MinStack() {
+        
+    }
+    
+    public void push(int val) {
+        if (st.size() == 0 && min.size() == 0){
+            st.push(val);
+            min.push(val);
+            return;
+        } 
+        st.push(val);
+        int temp = Math.min(val, min.peek());
+        min.push(temp);
+        
+    }
+    
+    public void pop() {
+        st.pop();
+        min.pop();
+    }
+    
+    public int top() {
+        return st.peek();
+    }
+    
+    public int getMin() {
+        return min.peek();
+    }
+}
+
