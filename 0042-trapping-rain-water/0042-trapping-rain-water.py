8d904e3ae50ed983dd1f8f09939dc22d6162fc75
Tanmoy Santra <108757431+Tanmoy037@users.noreply.github.com>
2025-06-12 23:40:01 +0530

    Time: 33 ms (14.45%), Space: 19.4 MB (39.08%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,7 +1,7 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        left, right = 0, len(height) - 1
-        left_max = right_max = 0
+        left ,right = 0, len(height) - 1
+        left_max , right_max = 0, 0
         water = 0
 
         while left < right:
@@ -17,6 +17,4 @@ class Solution:
                 else:
                     water += right_max - height[right]
                 right -= 1
-
         return water
-        
